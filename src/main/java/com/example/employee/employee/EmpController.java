808b3a6e8aee61475227ecaa3ab41d232e8cd4b6
ayushraj2925@gmail.com <ayushraj2925@gmail.com>
2026-07-26 23:49:29 +0530

    Update CORS

--- src/main/java/com/example/employee/employee/EmpController.java
+++ src/main/java/com/example/employee/employee/EmpController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("https://frontend-k461.onrender.com")
 public class EmpController {
 
     ////By using Object
